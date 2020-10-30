f5dbed3b4dd43af8c5a060fa61eb70d2421ef2a1
dench1k <denmoskvitin@gmail.com>
2020-10-30 18:26:04 +0200

    feat: implement transform draft

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  let modifiedArr = [];
+  for (let i = 0; i < arr.length; i++) {
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    if (arr[i] === '--discard-prev') {
+      if (modifiedArr[i - 1]) {
+        modifiedArr[i - 1] = '--discard-value';
+      }
+    } else if (arr[i] === '--discard-next') {
+      if (arr[i + 1]) {
+        modifiedArr.push('--discard-value');
+      }
+    } else if (arr[i] === '--double-next') {
+      modifiedArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      modifiedArr.push(arr[i - 1]);
+    } else {
+      if (arr[i - 1] === '--discard-next') {
+        modifiedArr.push('--discard-value');
+      } else {
+        modifiedArr.push(arr[i]);
+      }
+    }
+  }
+  return modifiedArr.filter(item => item !== '--discard-value' && item !== undefined);
+
 };
